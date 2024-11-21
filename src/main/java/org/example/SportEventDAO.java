bbe92cfb5c369c313f43bd5111aa91451d58e0ce
Lukáš <lukasko.varga@gmail.com>
2024-11-21 12:53:33 +0100

    oprava enumu

--- src/main/java/org/example/SportEventDAO.java
+++ src/main/java/org/example/SportEventDAO.java
@@ -38,7 +38,8 @@ public class SportEventDAO {
                 event.setEventName(record.getValue("event_name", String.class));
                 event.setStartTime(record.getValue("start_time", LocalDateTime.class));
                 event.setSportType(record.getValue("sport_type", String.class));
-                //event.setStatus(StatusForEvent.valueOf(record.getValue("status", String.class))); // Konverzia String na enum
+                event.setStatus(record.getValue("status", StatusForEvent.class));
+
 
 
                 events.add(event);
--- src/main/java/org/example/StatusForEvent.java
+++ src/main/java/org/example/StatusForEvent.java
@@ -1,6 +1,6 @@
 package org.example;
 
 public enum StatusForEvent {
-    UPCOMING,
-    FINISHED,
+    upcoming,
+    finished,
 }
