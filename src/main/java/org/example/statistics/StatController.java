56c218683fb54ce39c99950823531455344e52a3
Lukáš <lukasko.varga@gmail.com>
2024-12-08 22:22:14 +0100

    uprava statistiky

--- src/main/java/org/example/statistics/StatController.java
+++ src/main/java/org/example/statistics/StatController.java
@@ -30,11 +30,17 @@ public class StatController {
             // Display the statistics in the VBox
             for (Statistics stat : stats) {
                 Label totalBetsLabel = new Label("Total Bets: " + stat.getTotalBets());
+                totalBetsLabel.setStyle("-fx-font-size: 16px");
                 Label totalStakesLabel = new Label("Total Stakes: " + stat.getTotalStakes());
+                totalStakesLabel.setStyle("-fx-font-size: 16px");
                 Label totalWinningsLabel = new Label("Total Winnings: " + stat.getTotalWinnings());
+                totalWinningsLabel.setStyle("-fx-font-size: 16px");
                 Label winRateLabel = new Label("Win Rate: " + stat.getWinRate());
+                winRateLabel.setStyle("-fx-font-size: 16px");
                 Label averageBetLabel = new Label("Average Bet: " + stat.getAverageBet());
+                averageBetLabel.setStyle("-fx-font-size: 16px");
                 Label maxBetLabel = new Label("Max Bet: " + stat.getMaxBet());
+                maxBetLabel.setStyle("-fx-font-size: 16px");
 
                 // Add labels to the VBox
                 statBox.getChildren().addAll(
